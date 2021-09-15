aa67c214f0c0a6b922c18bd560848c62c51ed565
Jorge Lopez <je.lopez@me.com>
2021-09-14 23:19:30 -0700

    moved files

--- algorithms/canConstruct.js
+++
@@ -1,9 +0,0 @@
-// given a targetString and array of strings, return whether or not targetStr can be constructed by concatenating strings in given array
-//   strings in array may be reused
-//
-// m = targetStr.length
-// n = stringsArr.length
-
-const canConstruct1 = (targetStr, stringArr) => {
-
-};
